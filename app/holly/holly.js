5ff4a8f9aefcea83d2e75306bba13cf7dcae6744
Holly Becker <hbecker@artefactual.com>
2015-05-11 22:49:07 -0700

    Working through ng-book

--- app/holly/holly.js
+++ app/holly/holly.js
@@ -4,7 +4,7 @@ var hollyControllers = angular.module('hollyControllers', []);
 
 hollyControllers.controller('HollyController', ['$scope', '$timeout',
     function($scope, $timeout) {
-        $scope.clock = {}
+        $scope.clock = {};
         var updateClock = function () {
             $scope.clock.now = new Date();
             $timeout(function() {
@@ -12,5 +12,27 @@ hollyControllers.controller('HollyController', ['$scope', '$timeout',
             }, 1000);
         };
         updateClock();
+
+        $scope.counter = 0;
+        $scope.add = function(n) { $scope.counter += n; };
+        $scope.subtract = function(n) { $scope.counter -= n; };
+
+        $scope.person = {
+            name: 'Holly B',
+        };
+    }
+]);
+
+hollyControllers.controller('ParentController', ['$scope',
+    function($scope) {
+        $scope.person = {greeted: true};
+    }
+]);
+
+hollyControllers.controller('ChildController', ['$scope',
+    function($scope) {
+        $scope.sayHello = function() {
+            $scope.person.name = "Holly B";
+        };
     }
 ]);
